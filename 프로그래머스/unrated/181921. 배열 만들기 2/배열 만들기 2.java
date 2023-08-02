4387568293c87cb93dc54a91f4b93236be336b0f
HanByeol Seo <138543543+starshb@users.noreply.github.com>
2023-08-02 09:41:17 +0900

    [unrated] Title: 배열 만들기 2, Time: 27.30 ms, Memory: 90.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int l, int r) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = l; i<=r; i++) {
+            String str = i + "";
+            int count = 0;
+            for(int j = 0; j < str.length(); j++) {
+                if(str.charAt(j) == 48 || str.charAt(j) == 53) {
+                    count ++;
+                }
+            }
+            if(count == str.length()) {
+                list.add(i);
+            }
+        }
+        int[] answer = list.stream().mapToInt(i->i).toArray();
+        int[] empty = {-1};
+        if(answer.length == 0) return empty;
+        return answer;
+    }
+}

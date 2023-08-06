12facad1aa14c3d5747bb4db13f5265db4624f8a
HanByeol Seo <138543543+starshb@users.noreply.github.com>
2023-08-06 11:46:17 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i >= idx && arr[i] == 1) {
+                answer = i;
+                break;
+            } else {
+                answer = -1;
+            }
+        }
+        return answer;
+    }
+}

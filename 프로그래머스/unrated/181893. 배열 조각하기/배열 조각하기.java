24ce72b591b880f8ffaa1093fc248abf344bfa29
HanByeol Seo <138543543+starshb@users.noreply.github.com>
2023-08-08 09:48:39 +0900

    [unrated] Title: 배열 조각하기, Time: 0.09 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        for (int i = 0; i < query.length; i++) {
+            if (i % 2 == 0) {
+                arr = Arrays.copyOfRange(arr, 0, query[i]+1);
+            } else {
+                arr = Arrays.copyOfRange(arr, query[i], arr.length);
+            }
+        }
+        return arr;
+    }
+}

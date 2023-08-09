36a4dc9a201aea7773cb3ab8185d994972a38031
HanByeol Seo <138543543+starshb@users.noreply.github.com>
2023-08-09 09:53:46 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String[] str_list) {
+        String[] answer = {};
+        for (int i = 0; i < str_list.length;i++) {
+            if (str_list[i].equals("l")) {
+                return Arrays.copyOfRange(str_list, 0, i);
+            } else if (str_list[i].equals("r")) {
+                return Arrays.copyOfRange(str_list, i + 1, str_list.length);
+            }
+        }
+        return answer;
+    }
+}

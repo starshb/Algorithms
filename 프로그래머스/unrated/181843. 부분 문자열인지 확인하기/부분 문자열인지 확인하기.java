beba2478b169928bb78eb49cb69c95725c9163ea
HanByeol Seo <138543543+starshb@users.noreply.github.com>
2023-08-25 09:38:45 +0900

    [unrated] Title: 부분 문자열인지 확인하기, Time: 0.03 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String my_string, String target) {
+        int answer = 0;
+        if(my_string.contains(target)){
+            answer = 1;
+        }
+        return answer;
+    }
+}

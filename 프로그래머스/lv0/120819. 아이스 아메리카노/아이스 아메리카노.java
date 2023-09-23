212c426e731e9a46175c7115dbcb9846b411c848
HanByeol Seo <138543543+starshb@users.noreply.github.com>
2023-09-24 00:45:02 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        answer[0] = money / 5500;
+        answer[1] = money % 5500;
+        return answer;
+    }
+}

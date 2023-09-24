95e416b569de2df07b864cbc5dbaa12e1d982e48
HanByeol Seo <138543543+starshb@users.noreply.github.com>
2023-09-25 00:53:44 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int price) {
+        if (price >= 500000) {
+            price *= 0.8;
+        } else if (price >= 300000) {
+            price *= 0.9;
+        } else if (price >= 100000) {
+            price *= 0.95;
+        }
+        return price;
+    }
+}

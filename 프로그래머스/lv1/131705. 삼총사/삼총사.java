412d5e1d185be62ce447542e932f905be4b6294b
HanByeol Seo <138543543+starshb@users.noreply.github.com>
2023-10-06 00:48:03 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 66.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        
+        for(int i = 0; i < number.length; i++) {
+            for(int j = i + 1; j < number.length; j++) {
+                for(int k = j + 1; k < number.length; k++) {
+                    if(number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}

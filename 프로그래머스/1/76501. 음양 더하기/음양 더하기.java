d4199cb8c2921aa4335bead657b40d7156d19f27
HanByeol Seo <138543543+starshb@users.noreply.github.com>
2023-10-10 15:40:20 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for (int i = 0; i < signs.length; i++) {
+            if (signs[i]) {
+                answer += absolutes[i];
+            } else {
+                answer -= absolutes[i];
+            }
+        }
+        
+        return answer;
+    }
+}

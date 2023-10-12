420fc02d9dbbce6598ff6fcb67a73ba99bef6af9
HanByeol Seo <138543543+starshb@users.noreply.github.com>
2023-10-13 00:04:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 275.72 ms, Memory: 95.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        for(int i=0;i<completion.length;i++){
+            if(participant[i].equals(completion[i])==false){
+                return participant[i];
+            }
+        }        
+        return participant[participant.length-1];
+    }
+}

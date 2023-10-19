025bd84ca0353dd36c6ca55c182d3ef9c34096b3
HanByeol Seo <138543543+starshb@users.noreply.github.com>
2023-10-20 00:01:00 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 2.03 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] str = s.split("");
+        
+        int idx = 0;
+        for(int i=0; i<str.length; i++) {
+            if(str[i].equals(" ")) {
+                idx = 0;
+            } else if(idx % 2 == 0) {
+                str[i] = str[i].toUpperCase();
+                idx++;
+            } else if(idx % 2 != 0) {
+                str[i] = str[i].toLowerCase();
+                idx++;
+            }
+            answer += str[i];
+        } 
+        
+        return answer;
+    }
+}

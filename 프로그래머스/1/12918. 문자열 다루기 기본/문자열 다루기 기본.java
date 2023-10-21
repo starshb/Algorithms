f276977f0a145d61b08a6830b907f680c53adf38
HanByeol Seo <138543543+starshb@users.noreply.github.com>
2023-10-22 00:05:20 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean solution(String s) {
+        if(s.length() != 4 && s.length() != 6) return false;
+        for (int i=0;i<s.length();i++) {
+            if (s.charAt(i) < '0' || s.charAt(i) > '9') return false;
+        }        
+        return true;
+    }
+}

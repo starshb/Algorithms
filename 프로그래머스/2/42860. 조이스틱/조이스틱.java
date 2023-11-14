53c69fcd22ccb4816b0e62903f96ee68b9352f4a
HanByeol Seo <138543543+starshb@users.noreply.github.com>
2023-11-15 00:36:34 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        int length = name.length();
+
+        int index; // 다음 값들을 확인할 때 사용
+        int move = length - 1; // 좌우 움직임 수를 체크
+
+        for(int i = 0; i < name.length(); i++){
+            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+
+            index = i + 1;
+            // 연속되는 A 갯수 확인
+            while(index < length && name.charAt(index) == 'A'){
+                index++;
+            }
+
+            // 순서대로 가는 것과, 뒤로 돌아가는 것 중 이동수가 적은 것을 선택
+            move = Math.min(move, i * 2 + length - index);
+            // 2022년 이전 테스트 케이스만 확인하면 여기까지해도 정답처리가 되기 때문에, 이전 정답들에는 여기까지만 정리되어 있지만,
+            // BBBBAAAAAAAB 와 같이, 처음부터 뒷부분을 먼저 입력하는 것이 더 빠른 경우까지 고려하려면 아래의 코드가 필요합니다.
+            move = Math.min(move, (length - index) * 2 + i);
+        }
+        return answer + move;
+    }
+}

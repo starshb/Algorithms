667ead747499fb09f35fbfde773397d682c282e7
HanByeol Seo <138543543+starshb@users.noreply.github.com>
2023-11-16 00:56:36 +0900

    [level 2] Title: 가장 큰 수, Time: 1.67 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Solution {
+    public String solution(int[] numbers) {
+        String[] arr = new String[numbers.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        
+        if (arr[0].equals("0")) {
+           return "0";
+        }
+
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < arr.length; i++) {
+            answer.append(arr[i]);
+        }
+
+
+        return answer.toString();
+    }
+}

936cd11d4865deaba084a2e85d8eaf646a421b58
HanByeol Seo <138543543+starshb@users.noreply.github.com>
2023-11-27 00:29:19 +0900

    [level 2] Title: 숫자의 표현, Time: 1.21 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,20 @@
+class Solution {
+      public int solution(int n) {
+          int answer = 0;
+          
+          for(int i=1; i<=n; i++) {
+              int sum = 0;
+              for(int j=i; j<=n; j++) {
+                  sum += j;
+                  
+                  if(sum==n) {
+                      answer++;
+                      break;
+                  } else if(sum>n) {
+                      break;
+                  }
+              }
+          }      
+          return answer;
+      }
+    }

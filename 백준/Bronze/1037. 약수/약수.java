4688be6d2954ba754374a103aaada035c89bf1f6
HanByeol Seo <138543543+starshb@users.noreply.github.com>
2024-06-16 14:03:19 +0900

    [Bronze I] Title: 약수, Time: 112 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            min = Math.min(min, arr[i]);
+            max = Math.max(max, arr[i]);
+        }
+        System.out.print(min * max);
+    }
+}
